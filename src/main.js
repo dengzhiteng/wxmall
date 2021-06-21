ce0f8546968159dd1957ffb0a1ed284beb67757f
dengzhiteng <dengzhiteng@jwx.com.cn>
2021-06-21 17:18:57 +0800

    公众号项目

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+
+import { Button } from 'vant';
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(Button)
+    .mount("#app");
