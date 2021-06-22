c74ff5e950b23f7218340a66b158120eb329f51a
dengzhiteng <dengzhiteng@jwx.com.cn>
2021-06-22 17:16:27 +0800

    引入prettierrc

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about p1123123123ag11e</h1>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,21 +4,21 @@
       <van-cell title="单元格" value="内容" />
       <van-cell title="单元格" value="内容" label="描述信息" />
     </van-cell-group>
-    <p class="desc">test</p>  
+    <p class="desc">test</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: 'Home',
   components: {},
 };
 </script>
-<style  lang="scss" scoped>
+<style lang="scss" scoped>
 .desc {
   color: red;
-  font-size:14px;
-  width:200px;
-  margin:0;
+  font-size: 14px;
+  width: 200px;
+  margin: 0;
 }
 </style>
